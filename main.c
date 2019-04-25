88c1f8b847f1616d7360977535410d4bd89138f3
Jamie Quick <c3331585@uon.edu.au>
2019-04-25 12:20:26 +0000

    Outline

--- main.c
+++ main.c
@@ -14,11 +14,26 @@ int main() {
     char selection;
     scanf("%c", &selection);
     do {
+        switch(selection){
+            case 'a': // function call for rotation encryption
+                break;
+            case 'b': // function call for rotation decryption
+                break;
+            case 'c': // function call for substitution encryption
+                break;
+            case 'd': // function call for substitution decryption
+                break;
+            case 'e': // function call for substitution encryption w/key
+                break;
+            case 'f': // function call for substitution decryption w/key
+                break;
+            default: printf("unknown option, enter a, b, c, d, e or f");
+        }
         
         // block of code that calls above functions
         // switch case - call different function for each option...
         
-    } while(selection < 'a' || selection > 'f')
+    } while(selection < 'a' || selection > 'f');
     
   /*  
     FILE *input;
